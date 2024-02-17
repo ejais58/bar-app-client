1b1b31e5fd10b5a28667eff8e3e538efef78767f
Eliasjais <73757035+ejais58@users.noreply.github.com>
2024-02-17 19:22:51 -0300

    feat/home
    se realizo la pantalla home con su footer

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './page/home/home.component';
+import {MatCardModule} from '@angular/material/card';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { HomeComponent } from './page/home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
